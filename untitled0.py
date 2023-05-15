ce2918cc01d515a70656a482826d3a66cfbaa510
Khushi Agarwal <khushiagarwal226@gmail.com>
2023-05-15 21:39:35 +0530

    Modified

--- untitled0.py
+++ untitled0.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul  7 15:29:20 2022
+Created on Wed May 10 22:51:10 2023
 
 @author: khushiagarwal
 """
 
+
+from flask import Flask, render_template, request, redirect
+
+import sqlite3
+
+import threading
+import sqlite3
 
 from flask import Flask, request, render_template
 import pandas as pd
@@ -20,13 +27,67 @@ from io import BytesIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
+# Create a thread-local storage object for the database connection and cursor
+local = threading.local()
 
-# Declare a Flask app
+def get_conn():
+    """
+    Create a new database connection for the current thread, or return an existing one.
+    """
+    if not hasattr(local, 'conn'):
+        local.conn = sqlite3.connect('/Users/khushiagarwal/Desktop/users.db')
+    return local.conn
+
+def get_cursor():
+    """
+    Create a new database cursor for the current thread, or return an existing one.
+    """
+    if not hasattr(local, 'cursor'):
+        local.cursor = get_conn().cursor()
+    return local.cursor
+
+
+
+
+
+
 app = Flask(__name__)
 
-# Main function here
-# ------------------
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
 
+    # Insert the new user data into the database
+    c = get_cursor()
+    c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    # Commit the changes to the database
+    user=c.fetchone()
+    password=c.fetchone()
+    get_conn().commit()
+    if(user and password):
+        return render_template('index.html', msg="Successfully signed in,now you can login!!")
+    else:
+        return render_template('index.html', msg="Try again!")
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    # Query the database for the user with the given username and password
+    c = get_cursor()
+    c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+    user = c.fetchone()
+    password=c.fetchone()
+    error="Incorrect username or password"
+    if user is None:
+        return render_template('index.html', error=error)
+    else:
+        return main()
 consumer_key = "FoxhZf5AFjWHhDBggBkHgpbrx"
 consumer_secret = "kLnB5EjssvyMYYbbH1C13YJScOmy310OUDCuDANKiqYq9dHtqM"
 access_key= "1596558961758859264-jFbu3uG6pN8JeAL4NAkVlQqXfLGMJt"
@@ -41,23 +102,7 @@ df=pd.DataFrame()
 def main():
     
     
-    # If a form is submitted
-    """
-    if request.method == "POST":
-       
-       search_query = request.form.get("search")
-       tweets = tweepy.Cursor(api.search_tweets,
-              q=search_query,
-              lang="en",
-              ).items(50)
-       tweets_copy=clean(tweets)
-       df= pola(tweets_copy)
-       plot2(df)
-       #print(df[0][0])
-      # li=list(df.itertuples(index=False))
-       #print(df.to_string())
-       return df.to_string()
-      """
+  
        
        
     return render_template("newindex.html")
@@ -103,20 +148,5 @@ if __name__ == '__main__':
     app.run(debug = True)
     
 
-    
-    """
-       # Unpickle classifier
-        clf = joblib.load("classifier.pkl")
-        
-        # Get values through input bars
-        hastag = request.form.get("search_query")
-       
-        # Put inputs to dataframe
-        X = pd.DataFrame([[hastag]], columns = ["result"])
-        
-        # Get prediction
-        prediction = "uygbuhbhub"
-        
-    else:
-        prediction = "ihuibuin"
-        """
+if __name__ == '__main__':
+    app.run(debug=True)
